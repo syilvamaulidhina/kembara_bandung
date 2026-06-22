e02c09e5f36374cbf53045685ba32e261112bf59
Syilva <syilvamaulidhina@student.telkomuniversity.ac.id>
2026-06-22 12:51:05 +0700

    feat: add Maze snippet for UAT tracking in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,29 @@ export default function RootLayout({
           id="maze-snippet"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `PASTE_MAZE_SNIPPET_CONTENT_HERE`,
+            __html: `(function (m, a, z, e) {
+  var s, t, u, v;
+  try {
+    t = m.sessionStorage.getItem('maze-us');
+  } catch (err) {}
+  if (!t) {
+    t = new Date().getTime();
+    try {
+      m.sessionStorage.setItem('maze-us', t);
+    } catch (err) {}
+  }
+  u = document.currentScript || (function () {
+    var w = document.getElementsByTagName('script');
+    return w[w.length - 1];
+  })();
+  v = u && u.nonce;
+  s = a.createElement('script');
+  s.src = z + '?apiKey=' + e;
+  s.async = true;
+  if (v) s.setAttribute('nonce', v);
+  a.getElementsByTagName('head')[0].appendChild(s);
+  m.mazeUniversalSnippetApiKey = e;
+})(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '0379f5cf-938e-4307-b1a3-df60fad894c6');`,
           }}
         />
       </head>
